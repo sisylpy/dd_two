630049d7c5308192a6402016c21d687579dc656c
peiyiLi <454926763@qq.com>
2024-05-20 22:53:11 +0800

    try one

--- src/main/java/com/nongxinle/controller/NxCommunityOrdersSubController.java
+++ src/main/java/com/nongxinle/controller/NxCommunityOrdersSubController.java
@@ -131,7 +131,7 @@ public class NxCommunityOrdersSubController {
             entity.setNxCosStatus(9); //锁定状态
             nxCommunityOrdersSubService.update(entity);
         }
-        System.out.println("ddss");
+        System.out.println("ddss啊");
 
 //        map.put("status",9);
 //        List<NxCommunityPrintOrdersSubEntity> subEntitiesNine = nxCommunityOrdersSubService.queryPrintSubOrders(map);
